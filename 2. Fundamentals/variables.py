e085600e358f6b545b4306a492992892ceedad4d
aryandive <aryandive07@gmail.com>
2026-07-02 21:46:42 +0530

    today I revise stirng, loop, control, fundamental and more

---
+++ 2. Fundamentals/variables.py
@@ -0,0 +1,9 @@
+# Python is a dynamically typed language, meaning we don't need to declare the type of a variable.
+
+age = 20
+name = "Aryan"
+cgpa = 4.55
+
+
+for i in range(1, 11):
+    print("5 X ", i, " = ", 5*i)
